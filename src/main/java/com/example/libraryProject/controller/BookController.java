56d27a9aa7880c24af5f549af8106dcf55440c43
Elif Sude Ozmen <elifsude035@gmail.com>
2025-05-22 00:40:25 +0300

    Testing Creating A Book

--- src/main/java/com/example/libraryProject/controller/BookController.java
+++ src/main/java/com/example/libraryProject/controller/BookController.java
@@ -13,6 +13,7 @@ import java.util.List;
 
 @RequiredArgsConstructor
 @RequestMapping("/api/books")
+@RestController
 
 
 public class BookController {
--- src/test/java/com/example/libraryProject/LibraryProjectApplicationTests.java
+++ src/test/java/com/example/libraryProject/LibraryProjectApplicationTests.java
@@ -1,13 +1,32 @@
 package com.example.libraryProject;
 
+import com.example.libraryProject.model.Book;
+import com.example.libraryProject.repository.BookRepository;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
+import java.time.LocalDate;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@ActiveProfiles("mysql")
 @SpringBootTest
 class LibraryProjectApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+	@Autowired
+	private BookRepository bookRepository;
 
+	@Test
+	void testCreateBook() {
+		Book book = Book.builder()
+				.title("deneme 1")
+				.author("author")
+				.genre("genre")
+				.publishDate(LocalDate.of(2003,06,14))
+				.build();
+
+		Book savedBook = bookRepository.save(book);
+		assertThat(savedBook.getId()).isNotNull();}
 }
