0e03d70d60f4b0c549ff7e260017cf1b99aebed5
Elif Sude Ozmen <elifsude035@gmail.com>
2025-05-22 15:51:12 +0300

    Tests

--- src/test/java/com/example/libraryProject/LibraryProjectApplicationTests.java
+++ src/test/java/com/example/libraryProject/LibraryProjectApplicationTests.java
@@ -24,9 +24,72 @@ class LibraryProjectApplicationTests {
 				.title("deneme 1")
 				.author("author")
 				.genre("genre")
-				.publishDate(LocalDate.of(2003,06,14))
+				.publishDate(LocalDate.of(2003, 06, 14))
 				.build();
 
 		Book savedBook = bookRepository.save(book);
-		assertThat(savedBook.getId()).isNotNull();}
+		assertThat(savedBook.getId()).isNotNull();
+	}
+
+	@Test
+	void testCreateBook2(){
+		Book book = Book.builder()
+				.title("2. kitap")
+				.author("a")
+				.genre("g")
+				.publishDate(LocalDate.of(2002, 04, 12))
+				.build();
+
+		Book savedBook = bookRepository.save(book);
+		assertThat(savedBook.getId()).isNotNull();
+
+	}
+
+	@Test
+	void testDelete(){
+		Book book = Book.builder()
+				.title("silcez")
+				.author("a")
+				.genre("g")
+				.publishDate(LocalDate.of(2003, 06, 14))
+				.build();
+		Book savedBook = bookRepository.save(book);
+
+		assertThat(savedBook.getId()).isNotNull();
+
+
+		Long bookID = savedBook.getId();
+		bookRepository.deleteById(bookID);
+
+		assertThat(bookRepository.findById(bookID)).isNotPresent();
+
+	}
+
+
+	@Test
+	void testUpdate(){
+		Book book = Book.builder()
+				.title("updatelenmemiş hali")
+				.author("a")
+				.genre("g")
+				.publishDate(LocalDate.of(2003, 06, 14))
+				.build();
+		Book savedBook = bookRepository.save(book);
+		long bookID = savedBook.getId();
+		savedBook.setTitle("güncellenmiş hali");
+		savedBook.setAuthor("yeni author");
+		savedBook.setGenre("yeni genre");
+
+		Book updated = bookRepository.save(savedBook);
+		assertThat(updated.getId()).isEqualTo(bookID);
+		assertThat(updated.getTitle()).isEqualTo("güncellenmiş hali");
+
+
+
+
+
+
+	}
+
+
 }
